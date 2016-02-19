e1b528c245b341e4685c6e6564443396af6faf71
Leon Akasaka <leon@wisdomsoft.jp>
2016-02-20 03:01:06 +0900

    Add: Projects and interfaces.

---
+++ Levolution.RhythmGame.Models.Shared/IMusicalScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Levolution.RhythmGame.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IMusicalScore<TTarget>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        IEnumerable<INote<TTarget>> Notes { get; }
+    }
+}
---
+++ Levolution.RhythmGame.Models.Shared/INote.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Levolution.RhythmGame.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TNote"></typeparam>
+    /// <typeparam name="TOrientation"></typeparam>
+    public interface INote<TTarget>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        TimeSpan StartTime { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        TimeSpan? EndTime { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        TTarget Target { get; }
+    }
+}
